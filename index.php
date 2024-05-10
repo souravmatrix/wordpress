84839d9d0042dd78d568001518b94ffbfdf2454e
Sourav Banerjee <souravbanerjee@matrixnmedia.com>
2024-05-10 16:47:47 +0530

    add new index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header> This is my new header</header>
+    <p> Content willl come here</p>
+    <footer>This is my awesome footer</footer>
+</body>
+
+</html>
